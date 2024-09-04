8ce5f55b0ff9af572f1a4d13d54b44b1d9305489
Kemsara Hasaka Pathirana <139368574+KemsaraPathirana@users.noreply.github.com>
2024-09-04 21:13:13 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './layout';
+import MyTabs from './MyTabs';
+
+const Index = () => {
+    return (
+        <Layout>
+            <MyTabs/> 
+        </Layout>              
+    );  
+};
+
+export default Index;
